36d1eb3eb3ddb264192649d7c8a868603dc237a6
Heo <Heo@163.com>
2022-09-12 19:28:58 +0800

    first commit

---
+++ src/main/java/com/heo/GitTest.java
@@ -0,0 +1,13 @@
+package com.heo;
+
+/**
+ * @author Heo
+ * @version 1.0
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,heo");
+    }
+
+}
